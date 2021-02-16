47ba8de7f8f28bc167d2d05e4418ba9f5d773634
Rin (they/them) <celanthe@users.noreply.github.com>
2021-02-16 10:42:07 -0600

    Update index.js
    Updated index.js with code we have so far to add labels to issues

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
-/**
- * This is the main entrypoint to your Probot app
- * @param {import('probot').Probot} app
- */
-module.exports = (app) => {
-  // Your code here
-  app.log.info("Yay, the app was loaded!");
-
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    return context.octokit.issues.createComment(issueComment);
+  
+var commands = require('probot-commands');
+module.exports = app => {
+  // Your code here //gfg
+  app.log("Yay! The app was loaded!");
+  // example of probot responding 'Hello World' to a new issue being opened
+  app.on("issues.opened", async context => {
+    const params = context.issue({ body: "Hello World!" });
+    return context.github.issues.createComment(params);
   });
+  app.on("issue_comment.created", async context => { //gfg
+    commands(app, "lm", (context, command) => {
+      const labelvals = command.arguments.split(/, */);
+      // var labelipjson = JSON.stringify(labelvals);
+      var label_len = labelvals.length;
+      context.log('label_len ' + label_len);
+      var labels = labelvals.slice(1, label_len);
+      
+      context.log(labels); //gfg
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+      if (labelvals[0] == "add") {
+        // var labels = labelvals.slice(1, label_len);
+        context.log(labels);
+        return context.github.issues.addLabels(context.issue({ labels }));
+      } 
+      else if (labelvals[0] == "remove") {
+      for (var i = 0; i < label_len; i++) {
+        // context.log(labels);
+        return context.github.issues.removeLabel(
+          context.issue({ name: labels[i] })
+        );
+      }
+      }else { //gfg
+        var params = context.issue({
+          body:
+            "improper command. follow the syntax: /lm [add/remove] label1 label2 label3"
+        });
+        return context.github.issues.createcomment(params);
+      }
+    });
+  });
 };
