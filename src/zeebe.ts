8df7a5e57dd04f6dd0ae3a595176c5400820c840
Josh Wulf <josh@magikcraft.io>
2021-03-05 23:50:04 +1300

    Meaningless push to trigger GitHub Actions
    Signed-off-by: Josh Wulf <josh@magikcraft.io>

--- src/zeebe.ts
+++ src/zeebe.ts
@@ -1,9 +1,9 @@
 import { Context, Probot } from "probot";
 
 import { ZBClient } from "zeebe-node";
-// import { config } from "dotenv";
+import { config } from "dotenv";
 
-// config(); // Read Camunda Cloudcredentials from .env file
+config(); // Read Camunda Cloudcredentials from .env file
 const zbc = new ZBClient();
 
 interface NewRepoProcessPayload {
