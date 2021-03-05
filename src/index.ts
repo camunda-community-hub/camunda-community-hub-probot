2c59dedf6c1cbb0fb0d1f16557a7e70317dd7c62
Josh Wulf <josh@magikcraft.io>
2021-03-05 13:39:44 +1300

    TypeScript refactor + Camunda Cloud integration
    Co-authored-by: Rin (they/them) <celanthe@users.noreply.github.com>
    Signed-off-by: Josh Wulf <josh@magikcraft.io>

---
+++ src/index.ts
@@ -0,0 +1,100 @@
+import { Probot } from "probot";
+import commands from "probot-commands";
+import { extractLabels } from "./extractLabels";
+
+module.exports = (app: Probot) => {
+  // Inject Probot app into Zeebe controller, and get a ZBClient for use here
+  const zbc = require("./zeebe")(app);
+
+  app.log("Yay! The app was loaded!");
+
+  app.webhooks.onAny(async (context) => {
+    // console.log('onAny', { event: context.name, action: context.payload.action, context: context });
+  });
+
+  app.on("installation_repositories", (context) => {
+    if (context.payload.action === "added") {
+      // How do we discriminate between a transfer and a straight-up new repo?
+      const { payload } = context;
+      const repositories_added: { name: string }[] = payload.repositories_added;
+      const sender = payload.sender;
+      const repolist = repositories_added.map((repo) => repo.name).join(",");
+      context.log.info(`${sender!.login} just created ${repolist}`);
+      repositories_added.forEach((repo) =>
+        zbc.createWorkflowInstance("new-repo-flow", {
+          sender: sender!.login,
+          repo: repo.name,
+        })
+      );
+    } else if (context.payload.action === "removed") {
+      const { payload } = context;
+      const repositories_removed: { name: string }[] =
+        payload.repositories_removed;
+      const { sender } = payload;
+      const repolist = repositories_removed.map((repo) => repo.name).join(",");
+      context.log.info(`${sender!.login} just removed ${repolist}`);
+      repositories_removed.forEach((repo) =>
+        zbc.publishMessage({
+          correlationKey: repo,
+          name: "repo-removed",
+          timeToLive: 0,
+        })
+      );
+    }
+  });
+
+  app.on("issues.opened", async (context) => {
+    // const params = context.issue({ body: "Hello World!" });
+    // return context.octokit.issues.createComment(params);
+  });
+
+  app.on("issue_comment.created", async (context) => {
+    //gfg
+    context.log.info("Issue comment created callback");
+  });
+
+  /** lm command processor */
+  commands(
+    app,
+    "lm",
+    (context: ProbotCommandContext, command: ProbotCommand) => {
+      const { labels, cmd } = extractLabels(command.arguments);
+      if (labels.length === 0) {
+        context.log("No labels specified. Received:", command.arguments);
+        return;
+      }
+
+      if (cmd == "add") {
+        context.log("Adding labels:", labels);
+        return context.github.issues.addLabels(context.issue({ labels }));
+      } else if (cmd == "remove") {
+        const removeLabel = (name: string) =>
+          context.github.issues.removeLabel(context.issue({ name }));
+        labels.forEach(removeLabel);
+      } else {
+        //gfg
+        var params = context.issue({
+          body:
+            "Improper command. Follow the syntax: /lm [add/remove] label1 label2 label3",
+        });
+        return context.github.issues.createcomment(params);
+      }
+    }
+  );
+};
+
+// For more information on building apps:
+// https://probot.github.io/docs/
+
+// To get your app running against GitHub, see:
+// https://probot.github.io/docs/development/
+
+interface ProbotCommandContext {
+  github: any;
+  issue: any;
+  log: (message: string, ...messages: any[]) => void;
+}
+
+interface ProbotCommand {
+  arguments: string;
+}
---
+++ src/zeebe.ts
@@ -0,0 +1,228 @@
+import { Context, Probot } from "probot";
+
+import { ZBClient } from "zeebe-node";
+import { config } from "dotenv";
+
+config(); // Read Camunda Cloudcredentials from .env file
+const zbc = new ZBClient();
+
+interface NewRepoProcessPayload {
+  sender: string;
+  repo: string;
+}
+
+interface LabelUpdate {
+  color: string;
+  description: string | undefined;
+  name: string;
+}
+
+interface Label {
+  id: number;
+  node_id: string;
+  url: string;
+  name: string;
+  description: string | null;
+  color: string;
+  default: boolean;
+}
+
+interface LabelDictionary {
+  [name: string]: LabelUpdate;
+}
+/**
+ *
+ * @param {import('probot').Probot} app
+ */
+module.exports = (app: Probot) => {
+  const { octokit } = ((): Pick<Context<any>, "octokit"> =>
+    (app as any).state)();
+  const owner = "camunda-community-hub";
+
+  zbc
+    .deployWorkflow("bpmn/new_repo_flow.bpmn")
+    .then(console.log)
+    .catch(console.error);
+
+  zbc.createWorker<NewRepoProcessPayload>({
+    taskType: "give-maintainer-permissions",
+    taskHandler: async (job, complete) => {
+      const { repo, sender } = job.variables;
+      const req = {
+        repo,
+        username: sender,
+        owner,
+        permission: "admin" as const,
+      };
+      console.log("give-maintainer-permissions", req);
+      const res = await octokit.repos.addCollaborator(req);
+      complete.success({ maintainer_data: res });
+    },
+  });
+
+  zbc.createWorker({
+    taskType: "update-README",
+    taskHandler: async (job, complete) => {
+      const { repo, sender } = job.variables;
+      // Create a PR to add this badge to the README:
+      // https://img.shields.io/badge/Community%20Extension-An%20open%20source%20community%20maintained%20project-FF4700
+      complete.success();
+    },
+  });
+
+  zbc.createWorker({
+    taskType: "add-labels",
+    taskHandler: async (job, complete) => {
+      const LABEL_REPO = "camunda-community-hub-probot";
+      const { repo } = job.variables;
+
+      const templateLabels = (
+        await octokit.issues.listLabelsForRepo({
+          repo: LABEL_REPO,
+          owner,
+        })
+      ).data;
+
+      const makeLabelDictionary = (
+        prev: LabelDictionary,
+        label: Label
+      ): LabelDictionary => ({
+        ...prev,
+        [label.name]: {
+          color: label.color,
+          description: label.description || undefined,
+          name: label.name,
+        },
+      });
+
+      const templateDictionary: LabelDictionary = templateLabels.reduce(
+        makeLabelDictionary,
+        {}
+      );
+
+      const currentLabels = (
+        await octokit.issues.listLabelsForRepo({
+          repo,
+          owner,
+        })
+      ).data;
+
+      const currentLabelsDictionary: LabelDictionary = currentLabels.reduce(
+        makeLabelDictionary,
+        {}
+      );
+
+      const labelExists = (label: { name: string }) =>
+        currentLabels.reduce(
+          (prev, currentLabel) => currentLabel.name === label.name || prev,
+          false
+        );
+
+      const missingLabels = templateLabels
+        .filter((label) => !labelExists(label))
+        .map((label) => ({
+          ...label,
+          description: label.description ?? undefined,
+        }));
+
+      const newLabelsCreated = await Promise.all(
+        missingLabels.map((label) =>
+          octokit.issues
+            .createLabel({
+              repo,
+              owner,
+              name: label.name,
+              description: label.description,
+            })
+            .then((res) => res.data)
+        )
+      );
+
+      const newLabelsDictionary: LabelDictionary = newLabelsCreated.reduce(
+        makeLabelDictionary,
+        {}
+      );
+
+      const removeBlank = (
+        label: LabelUpdate | undefined
+      ): label is LabelUpdate => label !== undefined;
+
+      const updateLabels = templateLabels
+        .map((label) =>
+          newLabelsDictionary[label.name]
+            ? undefined
+            : currentLabelsDictionary[label.name]
+            ? {
+                name: label.name,
+                description: templateDictionary[label.name].description,
+                color: templateDictionary[label.name].color,
+              }
+            : undefined
+        )
+        .filter(removeBlank);
+
+      Promise.all(
+        updateLabels.map((label) =>
+          octokit.issues.updateLabel({
+            repo,
+            owner,
+            ...label,
+          })
+        )
+      );
+      complete.success({
+        addLabels: `Added ${Object.keys(newLabelsDictionary).join(",")}.`,
+      });
+    },
+  });
+
+  const b = ["a", undefined, "b"].filter((r) => r !== undefined);
+
+  zbc.createWorker({
+    taskType: "add-topic",
+    taskHandler: async (job, complete) => {
+      const LABEL = "community-supported";
+      const { repo } = job.variables;
+      const topics = await octokit.repos.getAllTopics({
+        owner,
+        repo,
+      });
+      const alreadyHasLabel = topics.data.names.reduce(
+        (prev, label) => label === LABEL || prev,
+        false
+      );
+      if (!alreadyHasLabel) {
+        const names = [...topics.data.names, LABEL];
+        await octokit.repos.replaceAllTopics({
+          names,
+          owner,
+          repo,
+        });
+      }
+      complete.success({
+        topics: alreadyHasLabel
+          ? `Already has ${LABEL} topic`
+          : `Added ${LABEL} topic`,
+      });
+    },
+  });
+
+  zbc.createWorker({
+    taskType: "slack-message",
+    taskHandler: async (job, complete) => {
+      const { repo, sender } = job.variables;
+      console.log(`${sender} transferred ${repo} into the Community Hub`);
+      complete.success();
+    },
+  });
+
+  zbc.createWorker({
+    taskType: "create-task",
+    taskHandler: async (job, complete) => {
+      const { repo, sender } = job.variables;
+      complete.success();
+    },
+  });
+
+  return zbc;
+};
